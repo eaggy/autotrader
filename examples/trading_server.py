764f9af730c266d16d522440dc779474320a1a36
Ihar Shchatsinin <70367607+eaggy@users.noreply.github.com>
2021-07-31 19:39:22 +0200

    Read broker's credentials from config file
    Read broker's credentials and budget from config file.

--- examples/trading_server.py
+++ examples/trading_server.py
@@ -1,17 +1,8 @@
 #!/opt/miniconda3/envs/flask/bin/python
 # -*- coding: utf-8 -*-
 """Example of trading server."""
-
-import os
 import sys
-import inspect
 import configparser
-
-currentdir = os.path.dirname(
-    os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
 from autotrader.setup_logger import logger
 from autotrader.infrastructure import TradingServer
 
@@ -27,12 +18,15 @@ try:
     PORT = int(config.get('LISTENER', 'PORT'))
     LISTENER_PASSWORD = config.get('LISTENER', 'PASSWORD')
 
+    BROKER_USER = config.get('DEGIRO', 'USER')
+    BROKER_PASSWORD = config.get('DEGIRO', 'PASSWORD')
+
 except Exception as e:
     logger.critical(e)
     sys.exit(-1)
 
 
-def trading_server(host, port, password):
+def trading_server(host, port, password, broker_user, broker_password, budget):
     """
     Create and run trading server.
 
@@ -44,6 +38,12 @@ def trading_server(host, port, password):
         Port which is being used by the Listener object.
     password : str
         Authentication key.
+    broker_user : str
+        Broker user.
+    broker_password : str
+        Broker password.
+    budget : float
+        Trading budget.
 
     Returns
     -------
@@ -53,7 +53,10 @@ def trading_server(host, port, password):
     # initialize trading server
     ts = TradingServer(host=host,
                        port=port,
-                       password=password)
+                       password=password,
+                       broker_user=broker_user,
+                       broker_password=broker_password,
+                       budget=budget)
 
     # run trading server
     ts.run()
@@ -62,4 +65,7 @@ def trading_server(host, port, password):
 if __name__ == "__main__":
     trading_server(host=HOST,
                    port=PORT,
-                   password=LISTENER_PASSWORD)
+                   password=LISTENER_PASSWORD,
+                   broker_user=BROKER_USER,
+                   broker_password=BROKER_PASSWORD,
+                   budget=31000.0)

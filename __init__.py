a713a63257194e9430c918121d55fb6ecfab7490
Ihar Shchatsinin <70367607+eaggy@users.noreply.github.com>
2021-07-31 19:45:01 +0200

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from autotrader.setup_logger import logger

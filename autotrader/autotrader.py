4dd64805e0052f3efab92607036d33b9d589a840
Ihar Shchatsinin <70367607+eaggy@users.noreply.github.com>
2021-08-02 21:53:53 +0200

    Modify method `trade()` of `Autotrader` class

--- autotrader/autotrader.py
+++ autotrader/autotrader.py
@@ -30,7 +30,7 @@ class Autotrader(Degiro):
             logger.error('Unexpected key in trading info.')
             return None
 
-    def trade(self,  isin='', transaction='', price=0.0, size=0):
+    def trade(self):
         """
         Execute a trade.
 
@@ -45,59 +45,55 @@ class Autotrader(Degiro):
         """
         for trading_data in self.trading_data:
             # get ISIN from trading data
-            if isin == '':
-                try:
-                    isin = trading_data['isin']
-                except KeyError:
-                    logger.error('Unexpected key in trading info '
-                                 '("isin" expected).')
-                    return None
+            try:
+                isin = trading_data['isin']
+            except KeyError:
+                logger.error('Unexpected key in trading info '
+                             '("isin" expected).')
+                continue
 
             # get transaction from trading data
-            if transaction == '':
-                try:
-                    transaction = trading_data['transaction']
-                except KeyError:
-                    logger.error('Unexpected key in trading info '
-                                 '("transaction" expected).')
-                    return None
+            try:
+                transaction = trading_data['transaction']
+            except KeyError:
+                logger.error('Unexpected key in trading info '
+                             '("transaction" expected).')
+                continue
 
             # get price from trading data
-            if price == 0.0:
-                try:
-                    price = float(trading_data['price'])
-                except KeyError:
-                    logger.error('Unexpected key in trading info '
-                                 '("price" expected).')
-                    return None
-                except ValueError:
-                    logger.error('The price is not valid')
-                    return None
+            try:
+                price = float(trading_data['price'])
+            except KeyError:
+                logger.error('Unexpected key in trading info '
+                             '("price" expected).')
+                continue
+            except ValueError:
+                logger.error('The price is not valid')
+                continue
 
             # get size from trading data
-            if size == 0:
-                try:
-                    size = float(trading_data['size'])
-                    # if size is quotient
-                    if size < 1.0:
-                        if price > 0.0:
-                            size = round(self.budget * size/price)
-                        else:
-                            return None
+            try:
+                size = float(trading_data['size'])
+                # if size is quotient
+                if size < 1.0:
+                    if price > 0.0:
+                        size = round(self.budget * size/price)
                     else:
-                        size = round(size)
-                except KeyError:
-                    logger.error('Unexpected key in trading info '
-                                 '("size" expected).')
-                    return None
-                except ValueError:
-                    logger.error('The size is not valid')
-                    return None
+                        continue
+                else:
+                    size = round(size)
+            except KeyError:
+                logger.error('Unexpected key in trading info '
+                             '("size" expected).')
+                continue
+            except ValueError:
+                logger.error('The size is not valid')
+                continue
 
             # check volume
             if price*size < self.budget/100.0:
                 logger.error('Position size is too small.')
-                return None
+                continue
 
             # get broker info
             self.login(self.user, self.password)
@@ -114,7 +110,7 @@ class Autotrader(Degiro):
             except Exception as e:
                 logger.error(e)
                 logger.warning('No product ID was found.')
-                return None
+                continue
 
             # check sell size
             if transaction == 'SELL':
@@ -127,9 +123,11 @@ class Autotrader(Degiro):
                         size = actual_size
                 except Exception as e:
                     logger.error(e)
-                    return None
+                    continue
 
             # execute trade
-            self.place_order(transaction, product_id, size,
-                             limit=price, stop_loss=None,
-                             order_type=0, validity=1)
+            print('Executing {} for {} of {} at {}.'.format(transaction, product_id, size, price))
+            #self.place_order(transaction, product_id, size,
+            #                 limit=price, stop_loss=None,
+            #                 order_type=0, validity=1)
+
